f1a42e666caeddf85fc2886edb2dc864e2a32ac8
Jayasree45 <gudlajayasri@gmail.com>
2022-10-14 08:25:13 +0000

    fixing the bug

---
+++ bug.java
@@ -0,0 +1,17 @@
+class Bug
+{
+	public static void main(String[] args)
+	{
+		int x = 5;
+		for (int i =0; i<= x; i++)
+		{
+			for(int j = 0; j <= i; j++)
+			{
+				System.out.print(" *");
+
+			}
+			System.out.println("");
+		}
+		System.out.println(" the out put of value x is printed");
+	}	
+}
