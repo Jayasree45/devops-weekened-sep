c4dfb3cfa1597422a2e6285f63c4dd72b87109ca
Jayasree45 <gudlajayasri@gmail.com>
2022-10-14 13:10:54 +0000

    Changes to check the Poll SCM

--- newfeature.java
+++ newfeature.java
@@ -2,6 +2,7 @@ class NewFeature
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello My new position");
+		System.out.println("Hello Jayasree Welcome to My new position");
+		System.out.println("Poll SCM");
 	}
 }
